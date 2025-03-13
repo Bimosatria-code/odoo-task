33f92d647e4905cbf27e1acd96d24bd34f77feb3
Bimosatria-code <bimosatria6@gmail.com>
2025-03-13 10:07:39 +0700

    fixing contact approval dan sales rental

--- sales_rental/models/product_template.py
+++ sales_rental/models/product_template.py
@@ -11,7 +11,17 @@ class ProductTemplate(models.Model):
         for rent in self:
             rent.count_rent = self.env['product.template'].search_count([('is_rent', '=', True)])
 
+
+    def get_sale_order_with_rent_products(self, product_id):
+        sale_orders = self.env['sale.order.line'].search([
+            ('product_template_id', '=', product_id)
+        ]).mapped('order_id')
+
+        return sale_orders.ids
+
+
     def action_open_sale_order(self):
+        order_ids = self.get_sale_order_with_rent_products(self.id)
         return {
             'type': 'ir.actions.act_window',
             'name': 'Sale Order',
@@ -21,5 +31,6 @@ class ProductTemplate(models.Model):
                 (self.env.ref('sale.view_order_tree').id, 'tree'),
                 (self.env.ref('sale.view_order_form').id, 'form'),
             ],
-
+            'domain': [('id', 'in', order_ids)],
+            # 'context': {'default_is_rent': True},
         }
--- sales_rental/models/sale_order.py
+++ sales_rental/models/sale_order.py
@@ -15,23 +15,53 @@ class SaleOrder(models.Model):
       ('cancelled', 'Cancelled'),
     ], default = 'draft', string='Rental Status')
 
-    @api.depends('rental_start_date', 'rental_return_date')
+    # @api.depends('rental_start_date', 'rental_return_date')
+    # def _compute_duration_days(self):
+    #     for rent in self:
+    #         if rent.rental_start_date and rent.rental_return_date:
+    #             duration = rent.rental_return_date - rent.rental_start_date
+    #             rent.duration_days = max(duration.days, 0)
+    #         else:
+    #             rent.duration_days = 0
+
+    @api.depends("rental_start_date", "rental_return_date")
     def _compute_duration_days(self):
         for rent in self:
             if rent.rental_start_date and rent.rental_return_date:
-                duration = rent.rental_return_date - rent.rental_start_date
-                rent.duration_days = max(duration.days, 0)
+                start_date = rent.rental_start_date.replace(hour=0, minute=0, second=0)
+                return_date = rent.rental_return_date.replace(hour=0, minute=0, second=0)
+                rent.duration_days = (return_date - start_date).days
             else:
                 rent.duration_days = 0
 
+
+
+    # def action_confirm(self):
+    #     res = super(SaleOrder, self).action_confirm()
+    #     for order in self:
+    #         if order.is_rental_order:
+    #             now = fields.Datetime.now()
+    #             if order.rental_start_date <= now <= order.rental_return_date:
+    #                 order.rental_status = 'reserved'
+    #     return res
+
     def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
+        result = super(SaleOrder, self).action_confirm()
+        today = fields.Datetime.now()
         for order in self:
-            if order.is_rental_order:
-                now = fields.Datetime.now()
-                if order.rental_start_date <= now <= order.rental_return_date:
-                    order.rental_status = 'reserved'
-        return res
+            if order.rental_start_date and order.rental_return_date:
+                if order.rental_start_date <= today <= order.rental_return_date:
+                    order.rental_status = "reserved"
+            else:
+                raise ValidationError(_("Start Date and Return Date can not be empty"))
+        return result
+
+    def action_rental_sales_confirm(self):
+        today = fields.Datetime.now()
+        for order in self:
+            if order.rental_start_date and order.rental_return_date:
+                if order.rental_start_date <= today <= order.rental_return_date:
+                    order.rental_status = "reserved"    
 
     def action_reserve(self):
         for order in self:
@@ -41,10 +71,9 @@ class SaleOrder(models.Model):
         for order in self:
             order.rental_status = 'returned'
 
-    @api.onchange('rental_return_date')
-    def _onchange_rental_return_date(self):
-        isPriorThanStartDate = self.rental_return_date >= self.rental_start_date
-        if not isPriorThanStartDate:
-            self.rental_return_date = False
-            return  {"warning": {"title": _("Warning"), "message": _("Please select a valid return date.")}}
-
+    # @api.onchange('rental_return_date')
+    # def _onchange_rental_return_date(self):
+    #     isPriorThanStartDate = self.rental_return_date >= self.rental_start_date
+    #     if not isPriorThanStartDate:
+    #         self.rental_return_date = False
+    #         return  {"warning": {"title": _("Warning"), "message": _("Please select a valid return date.")}}
